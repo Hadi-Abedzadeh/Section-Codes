f7855d528d307c04ff5856a671267f6787163f0f
Hadi Abedzadeh <Hadi-Abedzadeh@users.noreply.github.com>
2017-01-27 23:41:37 +0330

    Create cryptography.php

---
+++ cryptography.php
@@ -0,0 +1,21 @@
+<pre><?php 
+print_r( hash_algos());
+echo hash ('fnv164', 'dddd');
+
+echo hash('sha256',"sss"); 
+?> sdasdsad<pre><?php 
+print_r( hash_algos());
+echo hash ('fnv164', 'dddd');
+
+echo hash('sha256',"sss"); 
+?> sdasdsad<pre><?php 
+print_r( hash_algos());
+echo hash ('fnv164', 'dddd');
+
+echo hash('sha256',"sss"); 
+?> sdasdsad<pre><?php 
+print_r( hash_algos());
+echo hash ('fnv164', 'dddd');
+
+echo hash('sha256',"sss"); 
+?> 
